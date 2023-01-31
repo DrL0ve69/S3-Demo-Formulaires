14a49f25341ac871d6073b527a4c38fee331b0f9
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2023-01-31 10:39:51 -0500

    Ajouter le datalist et les messages d'erreurs sur la page.

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ function valider() {
     if (mdp1 === mdp2 && mdp1.length >= 8) {
         // On veut aussi avoir minimum 8 caractères
         // On veut aussi avoir des chiffres et des lettres
+        document.getElementById("erreur_mdp").setAttribute("class", "invisible");
         return true
     } else {
-        alert("Les 2 mots de passe doivent être identiques et contenir au moins 8 caractères")
+        if(mdp1 != mdp2){
+            document.getElementById("erreur_mdp").innerHTML = "Les 2 mots de passe doivent être <strong>identiques</strong>";
+        }
+        else if(mdp1.length < 8){
+            document.getElementById("erreur_mdp").innerHTML = "Le mot de passe doit contenir au moins <strong>8 caractères</strong>";
+        }
+        //alert("Les 2 mots de passe doivent être identiques et contenir au moins 8 caractères")
+        //Remplacer le pop-up par un message sur la page:
+        document.getElementById("erreur_mdp").setAttribute("class", "");
         return false; //Empêcher l'envoie du formulaire
     }
 }
