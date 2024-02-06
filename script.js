fdf33d656d8078211abfbc65e631bf6773252c8c
phili <0758510@cegepoutaouais.qc.ca>
2024-02-06 00:09:31 -0500

    1er reg x fonctionne

--- script.js
+++ script.js
@@ -5,15 +5,16 @@
 
 // Validation du nom d'utilisateur
 const texteNomUtilisateur = document.getElementById("nomUtilisateur");
-var nomUtilisateurEntre = document.getElementById("nomUtilisateur").value;
-var nomUtilisateurTest = /^(a-z|A-Z)+(0-9){0|1|2}$/.test(nomUtilisateurEntre);
+nomUtilisateurEntre = document.getElementById("nomUtilisateur").innerText;
+nomUtilisateurTest = /^[a-z]{0,20}[0-9]{0,2}$/.test(nomUtilisateurEntre);
+
 function NomValide(){
-    if(nomUtilisateurTest===false){ texteNomUtilisateur.validity = "false"; }
+    if(nomUtilisateurTest===false){ texteNomUtilisateur.validity = "false"; texteNomUtilisateur.ariaInvalid = "Nigauddemarde";}
     else { texteNomUtilisateur.validity= "true"; }
 }
 const texteAdresseCourriel = document.getElementById('adresseCourriel');
-const adresseCourrielEntre = document.getElementById("adresseCourriel").value;
-var adresseCourrielTest = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(adresseCourrielEntre);
+const adresseCourrielEntre = document.getElementById("adresseCourriel").innerText;
+adresseCourrielTest = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(adresseCourrielEntre);
 function EmailValide(){
     if(adresseCourrielTest===false) { texteAdresseCourriel.validity= "false"; }
     else{ adresseCourrielEntre.style.color = "green"; texteAdresseCourriel.style.borderColor="green"; }
@@ -21,7 +22,19 @@ function EmailValide(){
 function PasswordValide(){
     const passUtilisateur = document.getElementById("password").value;
     const passUtilisateurElement = document.getElementById("password");
-    if(passUtilisateur===nomUtilisateurEntre) {alert("Votre mot de passe est le même que le nom d'utilisateur "); }
+
+    if(passUtilisateur===nomUtilisateurEntre) {
+        passUtilisateurElement.validity = 'false';
+        passUtilisateurElement.popover = "Mot de passe invalide";
+        passUtilisateur.onfocus.showPopover();
+    } else passUtilisateurElement.validity ="true";
 }
+const allInputs = document.getElementsByTagName("input").valueOf();
+const formulaireComplet = document.getElementById('formulaireComplet');
+function FormulaireValide() {
+    if (allInputs.validity === "false"){ allInputs.focus(); document.getElementById('boutonSubmit').createElement('p',"Élément invalide") }
+}
+
+
 
 
